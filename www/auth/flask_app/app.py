0f8b1d8066201444f0d9930eec3e0fcab99f3883
Aditya Mitra <adityaarghya0@gmail.com>
2025-08-13 07:24:23 +0530

    Update app.py

--- www/auth/flask_app/app.py
+++ www/auth/flask_app/app.py
@@ -6,20 +6,59 @@ from flask import *
 
 from fido2.server import Fido2Server
 from fido2.webauthn import PublicKeyCredentialRpEntity, PublicKeyCredentialUserEntity
+from fido2.attestation import UntrustedAttestation
+from fido2.mds3 import MdsAttestationVerifier, parse_blob
 import base64 
 from cryptography.fernet import Fernet
 import time
 import json
+import requests
 
 app = Flask(__name__, static_url_path="")
 app.secret_key = os.urandom(32)  
 rp = PublicKeyCredentialRpEntity(name="Demo server", id="$$domainname$$")
 
 
-server = Fido2Server(rp)
+
 
 basepath='/var/cahicha'
 #os.system(f'mkdir -p {basepath}')
+
+ca = b64decode(
+    """
+MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G
+A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp
+Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4
+MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG
+A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI
+hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8
+RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT
+gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm
+KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd
+QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ
+XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw
+DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o
+LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU
+RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp
+jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK
+6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX
+mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs
+Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH
+WD9f"""
+)
+
+mds=None
+
+def getMds():
+    global mds
+    mdsurl='https://mds3.fidoalliance.org/'
+    resp=requests.get(mdsurl)
+    resptext=resp.content
+    metadata=parse_blob(resptext, ca)
+    mds=MdsAttestationVerifier(metadata)
+    
+getMds()
+server = Fido2Server(rp, attestation='direct', verify_attestation=mds,)
 
 secret={}
 
